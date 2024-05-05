e0065a9e8b41c8b2954b41b3487a3b05168754fd
Amay Dixit <amaydixit11@gmail.com>
2024-05-05 13:47:37 +0530

    new file:   .gitignore 	new file:   README.md 	new file:   app/favicon.ico 	new file:   app/globals.css 	new file:   app/layout.tsx 	new file:   app/page.tsx 	modified:   components/Lander.tsx 	new file:   next.config.mjs 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.mjs 	new file:   public/next.svg 	new file:   public/vercel.svg 	new file:   tailwind.config.ts 	new file:   tsconfig.json  	new file:   types/index.ts

--- components/Lander.tsx
+++ components/Lander.tsx
@@ -2,9 +2,7 @@ import Image from "next/image";
 import CustomButton from "./CustomButton";
 
 const Lander = () => {
-  function handleScroll(){
-    
-  }
+  const handleScroll = () => {}
   return (
     <div className = "lander flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto">
       <div className = "flex-1 pt-36 padding-x">
@@ -14,12 +12,19 @@ const Lander = () => {
         <p className = "subtitle text-[27px] text-black-100 font-light mt-5">
           Make your home a library with our lightning fast deliveries!
         </p>
-      </div>
       <CustomButton 
         title = "See Available Books"
         containerStyles = "bg-primary-blue text-white rounded-full mt-10"
         handleClick = {handleScroll}
       />
+      </div>
+      <div className = "image-container">
+        <div className = "image">
+          <Image src = "" alt = "books_pic" fill className = "object-contain" />
+          <div className = "image-overlay" />
+        </div>
+      </div>
+
 
     </div>
   )
